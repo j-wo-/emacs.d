941f05e82ddd823f5b447aed56d2ac0f813175d9
Jeff Workman <jeff.workman@gmail.com>
2015-05-16 10:48:34 -0400

    lisp-indent-function by default + reindent init.el

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 (setq vc-follow-symlinks t)
 
 (use-package uniquify
-    :config (setq uniquify-buffer-name-style 'post-forward))
+  :config (setq uniquify-buffer-name-style 'post-forward))
 
 (defun switch-to-theme (theme)
   (dolist (active-theme custom-enabled-themes)
@@ -44,38 +44,38 @@
 (use-package color-theme-sanityinc-solarized)
 
 (use-package auto-complete-config
-    :config
+  :config
   (ac-config-default)
   (setq global-auto-complete-mode t)
   (setq ac-use-dictionary-as-stop-words nil)
   (setq ac-use-fuzzy t)
   (define-globalized-minor-mode real-global-auto-complete-mode
-      auto-complete-mode (lambda ()
-			   (if (not (minibufferp (current-buffer)))
-			       (auto-complete-mode 1))))
+    auto-complete-mode (lambda ()
+			 (if (not (minibufferp (current-buffer)))
+			     (auto-complete-mode 1))))
   (real-global-auto-complete-mode t))
 
 (use-package projectile
-    :config
+  :config
   (projectile-global-mode)
   (setq projectile-enable-caching nil)
   (setq projectile-mode-line
 	'(:eval (format " [%s]" (projectile-project-name)))))
 
 (use-package smex
-    :bind ("M-x" . smex)
-    :config (smex-initialize))
+  :bind ("M-x" . smex)
+  :config (smex-initialize))
 
 (use-package scala-mode2
-    :mode ("\\.scala\\'" . scala-mode)
-    :init
+  :mode ("\\.scala\\'" . scala-mode)
+  :init
+  :config
+  (use-package ensime
     :config
-    (use-package ensime
-	:config
-      (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
+    (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
 
 (use-package ido
-    :config
+  :config
   (ido-mode 1)
   (setq ido-use-faces nil)
   (use-package flx-ido :config (flx-ido-mode 1))
@@ -86,7 +86,7 @@
 (use-package paren-face :config (global-paren-face-mode))
 
 (use-package paredit
-    :config
+  :config
   (diminish 'paredit-mode "()")
   (define-lisp-keys paredit-mode-map))
 
@@ -95,73 +95,70 @@
  (lambda ()
    (use-package ielm)
    (use-package elisp-slime-nav
-       :config (diminish 'elisp-slime-nav-mode "M-."))
+     :config (diminish 'elisp-slime-nav-mode "M-."))
    (define-lisp-keys emacs-lisp-mode-map)
    (paredit-mode)
    (turn-on-elisp-slime-nav-mode)))
 
 (use-package clojure-mode
-    :mode "\\.clj\\'" "\\.cljs\\'"
+  :mode "\\.clj\\'" "\\.cljs\\'"
+  :config
+  (define-lisp-keys clojure-mode-map)
+  (add-hook 'clojure-mode-hook 'paredit-mode)
+  (defface square-brackets
+    '((t (:foreground "#c0c43b"))) 'paren-face)
+  (defface curly-brackets
+    '((t (:foreground "#50a838"))) 'paren-face)
+  (defconst clojure-brackets-keywords
+    '(("\\[" 0 'square-brackets)
+      ("\\]" 0 'square-brackets)
+      ("[\\{\\}]" 0 'curly-brackets)))
+  (add-hook
+   'paren-face-mode-hook
+   (lambda ()
+     (if paren-face-mode
+	 (font-lock-add-keywords nil clojure-brackets-keywords t)
+       (font-lock-remove-keywords nil clojure-brackets-keywords))
+     (when (called-interactively-p 'any)
+       (font-lock-fontify-buffer))))
+  (use-package cider
     :config
-    (define-lisp-keys clojure-mode-map)
-    (add-hook 'clojure-mode-hook 'paredit-mode)
-    (defface square-brackets '((t (:foreground "#c0c43b"))) 'paren-face)
-    (defface curly-brackets '((t (:foreground "#50a838"))) 'paren-face)
-    (defconst clojure-brackets-keywords '(("\\[" 0 'square-brackets)
-					  ("\\]" 0 'square-brackets)
-					  ("[\\{\\}]" 0 'curly-brackets)
-					  ;;("\\}" 0 'curly-brackets)
-					  ))
-    (add-hook
-     'paren-face-mode-hook
-     (lambda ()
-       (if paren-face-mode
-	   (font-lock-add-keywords nil clojure-brackets-keywords t)
-	 (font-lock-remove-keywords nil clojure-brackets-keywords))
-       (when (called-interactively-p 'any)
-	 (font-lock-fontify-buffer))))
-    (use-package
-	cider
-	:config
-      (setq cider-lein-command "~/bin/lein")
-      (use-package
-	  ac-cider
-	  :config
-	(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-	(add-hook 'cider-mode-hook 'ac-cider-setup)
-	(add-hook 'cider-repl-mode-hook 'ac-cider-setup))
-      (use-package
-	  cider-eldoc
-	  :config (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode))
-      (setq cider-repl-popup-stacktraces t)
-      (setq cider-auto-select-error-buffer t)
-      (add-hook 'cider-mode-hook 'paredit-mode)
-      (add-hook 'cider-repl-mode-hook 'paredit-mode)
-      (define-lisp-keys cider-repl-mode-map)))
+    (setq cider-lein-command "~/bin/lein")
+    (use-package ac-cider
+      :config
+      (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+      (add-hook 'cider-mode-hook 'ac-cider-setup)
+      (add-hook 'cider-repl-mode-hook 'ac-cider-setup))
+    (use-package cider-eldoc
+      :config (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode))
+    (setq cider-repl-popup-stacktraces t)
+    (setq cider-auto-select-error-buffer t)
+    (add-hook 'cider-mode-hook 'paredit-mode)
+    (add-hook 'cider-repl-mode-hook 'paredit-mode)
+    (define-lisp-keys cider-repl-mode-map)))
 
 (use-package haskell-mode
-    :mode "\\.hs\\'" "\\.hs-boot\\'" "\\.lhs\\'" "\\.lhs-boot\\'"
+  :mode "\\.hs\\'" "\\.hs-boot\\'" "\\.lhs\\'" "\\.lhs-boot\\'"
+  :config
+  (use-package ghc)
+  (use-package ac-haskell-process
     :config
-    (use-package ghc)
-    (use-package
-	ac-haskell-process
-	:config
-      (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
-      (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
-      (add-to-list 'ac-modes 'haskell-interactive-mode))
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-    (autoload 'ghc-init "ghc" nil t)
-    (autoload 'ghc-debug "ghc" nil t)
-    (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-    (setq haskell-process-suggest-remove-import-lines t)
-    (setq haskell-process-auto-import-loaded-modules t)
-    (setq haskell-process-log t)
-    (setq haskell-process-type 'cabal-repl))
+    (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
+    (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
+    (add-to-list 'ac-modes 'haskell-interactive-mode))
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil t)
+  (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+  (setq haskell-process-suggest-remove-import-lines t)
+  (setq haskell-process-auto-import-loaded-modules t)
+  (setq haskell-process-log t)
+  (setq haskell-process-type 'cabal-repl))
 
 (use-package tramp
-    :config
+  :config
   (setq tramp-default-method "ssh")
   (add-to-list 'tramp-default-user-alist
 	       '("ssh" "\\`jeff-desktop\\'" "jeff"))
@@ -170,69 +167,69 @@
   (add-to-list 'tramp-default-user-alist
 	       '("ssh" "\\`server\\'" "jeff")))
 
-(use-package
-    slime-autoloads
-    :commands slime
-    :mode
-    ("\\.lisp\\'" . lisp-mode)
-    ("\\.asd\\'" . lisp-mode)
+(use-package slime-autoloads
+  :commands slime
+  :mode
+  ("\\.lisp\\'" . lisp-mode)
+  ("\\.asd\\'" . lisp-mode)
+  :config
+  (setq slime-contribs '(slime-fancy slime-tramp))
+  (use-package slime
     :config
-    (setq slime-contribs '(slime-fancy slime-tramp))
-    (use-package
-	slime
-	:config
-      (define-lisp-keys slime-mode-map)
-      (define-lisp-keys slime-repl-mode-map)
-      (add-hook 'lisp-mode-hook 'paredit-mode)
-      (add-hook 'slime-repl-mode-hook 'paredit-mode)
-  
-      (defvar sbcl-run-command "sbcl --dynamic-space-size 2000 --noinform")
-      (defvar ccl-run-command "ccl64 -K utf-8")
-      (defvar ecl-run-command "ecl")
-  
-      (setq inferior-lisp-program sbcl-run-command
-	    slime-net-coding-system 'utf-8-unix
-	    lisp-indent-function 'common-lisp-indent-function
-	    slime-complete-symbol-function 'slime-fuzzy-complete-symbol
-	    slime-fuzzy-completion-in-place t
-	    slime-enable-evaluate-in-emacs t
-	    slime-autodoc-use-multiline-p t)
-      (define-key slime-mode-map [(return)] 'paredit-newline)
-      (define-key slime-mode-map (kbd "C-c .") 'slime-next-note)
-      (define-key slime-mode-map (kbd "C-c ,") 'slime-previous-note)
+    (define-lisp-keys slime-mode-map)
+    (define-lisp-keys slime-repl-mode-map)
+    (add-hook 'lisp-mode-hook 'paredit-mode)
+    (add-hook 'slime-repl-mode-hook 'paredit-mode)
+    (add-hook 'lisp-mode-hook
+	      (lambda ()
+		(setq-local lisp-indent-function
+			    'common-lisp-indent-function)))
+    
+    (defvar sbcl-run-command "sbcl --dynamic-space-size 2000 --noinform")
+    (defvar ccl-run-command "ccl64 -K utf-8")
+    (defvar ecl-run-command "ecl")
+    
+    (setq inferior-lisp-program sbcl-run-command
+	  slime-net-coding-system 'utf-8-unix
+	  slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+	  slime-fuzzy-completion-in-place t
+	  slime-enable-evaluate-in-emacs t
+	  slime-autodoc-use-multiline-p t)
+    (define-key slime-mode-map [(return)] 'paredit-newline)
+    (define-key slime-mode-map (kbd "C-c .") 'slime-next-note)
+    (define-key slime-mode-map (kbd "C-c ,") 'slime-previous-note)
 
-      (defun slime-sbcl ()
-	(interactive)
-	(setq inferior-lisp-program sbcl-run-command)
-	(slime))
-      (defun slime-ccl ()
-	(interactive)
-	(setq inferior-lisp-program ccl-run-command)
-	(slime))
-      (defun slime-ecl ()
-	(interactive)
-	(setq inferior-lisp-program ecl-run-command)
-	(slime))
-      (defun kill-slime ()
-	(interactive)
-	(kill-buffer "*inferior-lisp*")
-	(cond
-	  ((equal inferior-lisp-program sbcl-run-command)
-	   (kill-buffer "*slime-repl sbcl*"))
-	  ((equal inferior-lisp-program ccl-run-command)
-	   (kill-buffer "*slime-repl ccl*"))))
+    (defun slime-sbcl ()
+      (interactive)
+      (setq inferior-lisp-program sbcl-run-command)
+      (slime))
+    (defun slime-ccl ()
+      (interactive)
+      (setq inferior-lisp-program ccl-run-command)
+      (slime))
+    (defun slime-ecl ()
+      (interactive)
+      (setq inferior-lisp-program ecl-run-command)
+      (slime))
+    (defun kill-slime ()
+      (interactive)
+      (kill-buffer "*inferior-lisp*")
+      (cond
+       ((equal inferior-lisp-program sbcl-run-command)
+	(kill-buffer "*slime-repl sbcl*"))
+       ((equal inferior-lisp-program ccl-run-command)
+	(kill-buffer "*slime-repl ccl*"))))
 
-      (add-to-list 'projectile-globally-ignored-modes "comint-mode")
-      (add-to-list 'projectile-globally-ignored-modes "slime-repl-mode")
+    (add-to-list 'projectile-globally-ignored-modes "comint-mode")
+    (add-to-list 'projectile-globally-ignored-modes "slime-repl-mode")
 
-      (use-package slime-annot)
-      (use-package
-	  ac-slime
-	  :config
-	(defun set-up-slime-ac-fuzzy ()
-	  (set-up-slime-ac t))
-	(add-hook 'lisp-mode-hook 'set-up-slime-ac-fuzzy)
-	(add-hook 'slime-repl-mode-hook 'set-up-slime-ac-fuzzy))))
+    (use-package slime-annot)
+    (use-package ac-slime
+      :config
+      (defun set-up-slime-ac-fuzzy ()
+	(set-up-slime-ac t))
+      (add-hook 'lisp-mode-hook 'set-up-slime-ac-fuzzy)
+      (add-hook 'slime-repl-mode-hook 'set-up-slime-ac-fuzzy))))
 
 (if (null (window-system))
     (require 'git-gutter)
@@ -241,22 +238,22 @@
 (diminish 'git-gutter-mode)
 
 (use-package magit
-    :init (setq magit-last-seen-setup-instructions "1.4.0")
-    :config (diminish 'magit-auto-revert-mode))
+  :init (setq magit-last-seen-setup-instructions "1.4.0")
+  :config (diminish 'magit-auto-revert-mode))
 
 (use-package smart-mode-line
-    :init (setq sml/theme 'powerline)
-    :config
-    (use-package smart-mode-line-powerline-theme)
-    (sml/setup)
-    (sml/apply-theme 'powerline)
-    (if (null window-system)
-	(progn
-	  (setq sml/name-width 38)
-	  (setq sml/mode-width 42))
+  :init (setq sml/theme 'powerline)
+  :config
+  (use-package smart-mode-line-powerline-theme)
+  (sml/setup)
+  (sml/apply-theme 'powerline)
+  (if (null window-system)
       (progn
 	(setq sml/name-width 38)
-	(setq sml/mode-width 62))))
+	(setq sml/mode-width 42))
+    (progn
+      (setq sml/name-width 38)
+      (setq sml/mode-width 62))))
 
 ;;(switch-to-theme 'zenburn)
 ;;(switch-to-theme 'sanityinc-tomorrow-night)
@@ -264,11 +261,12 @@
 ;;(switch-to-theme 'sanityinc-solarized-light)
 ;;(switch-to-theme 'sanityinc-solarized-dark)
 (load-theme 'sanityinc-tomorrow-night t)
+;;(sml/apply-theme 'powerline)
 
 (unless (null window-system)
   ;;(setq emacs-custom-font "Droid Sans Mono:pixelsize=18")
-  (set-frame-font "Source Code Pro:pixelsize=15"))
+  (set-frame-font "Source Code Pro:pixelsize=18"))
 
 (unless (null window-system)
-  (set-frame-height (selected-frame) 44)
+  (set-frame-height (selected-frame) 55)
   (set-frame-width (selected-frame) 100))
