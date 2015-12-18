e73e16b07d5e8f54bc3e386d90a468dc3296bfc0
Jeff Workman <jeff.workman@gmail.com>
2015-12-18 01:06:35 -0500

    tomorrow-night

--- variables.el
+++ variables.el
@@ -1,5 +1,6 @@
 (setq custom-font "Sauce Code Pro Semibold:pixelsize=15")
-(setq custom-emacs-theme 'moe-dark)
+;;(setq custom-emacs-theme 'moe-dark)
+(setq custom-emacs-theme 'sanityinc-tomorrow-night)
 
 ;;(setq custom-font "Source Code Pro Medium:pixelsize=15")
 ;;(setq custom-font "Liberation Mono Medium:pixelsize=15")
