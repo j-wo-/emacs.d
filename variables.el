5f1e7b861fa9c725a2f6c3654b6ae74e5fb39703
Jeff Workman <jeff.workman@gmail.com>
2016-01-07 04:27:28 -0500

    improve powerline theming

--- variables.el
+++ variables.el
@@ -1,6 +1,10 @@
-(setq custom-font "Sauce Code Pro Semibold:pixelsize=15")
+(setq custom-font "Sauce Code Pro Medium:pixelsize=25")
 ;;(setq custom-emacs-theme 'moe-dark)
-(setq custom-emacs-theme 'sanityinc-tomorrow-night)
+;;(setq custom-emacs-theme 'sanityinc-tomorrow-night)
+;;(setq custom-emacs-theme 'gruvbox)
+(if (null window-system)
+    (setq custom-emacs-theme 'sanityinc-tomorrow-night-rxvt)
+  (setq custom-emacs-theme 'sanityinc-tomorrow-night))
 
 ;;(setq custom-font "Source Code Pro Medium:pixelsize=15")
 ;;(setq custom-font "Liberation Mono Medium:pixelsize=15")
