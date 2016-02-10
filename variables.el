0148c631bb98b3df00e1bed13116337fce6bbdc6
Jeff Workman <jeff.workman@gmail.com>
2016-02-09 19:51:32 -0500

    update variables.el

--- variables.el
+++ variables.el
@@ -1,12 +1,12 @@
-(setq custom-font "Sauce Code Pro Medium:pixelsize=25")
-;;(setq custom-emacs-theme 'moe-dark)
-;;(setq custom-emacs-theme 'sanityinc-tomorrow-night)
-;;(setq custom-emacs-theme 'gruvbox)
-(if (null window-system)
-    (setq custom-emacs-theme 'sanityinc-tomorrow-night-rxvt)
-  (setq custom-emacs-theme 'sanityinc-tomorrow-night))
-
-;;(setq custom-font "Source Code Pro Medium:pixelsize=15")
-;;(setq custom-font "Liberation Mono Medium:pixelsize=15")
 (setq custom-frame-width 100)
 (setq custom-frame-height 58)
+
+(setq custom-font "Sauce Code Pro Medium:pixelsize=25")
+
+(setq custom-emacs-theme 'moe-dark)
+;;(setq custom-emacs-theme 'sanityinc-tomorrow-night)
+;;(setq custom-emacs-theme 'gruvbox)
+'(if (null window-system)
+     (setq custom-emacs-theme 'sanityinc-tomorrow-night-rxvt)
+   (setq custom-emacs-theme 'sanityinc-tomorrow-night))
+
