d4cab178890f6f1af1b9532284de5c966061aa8e
Jeff Workman <jeff.workman@gmail.com>
2016-07-25 08:20:04 -0400

    Add functions for indenting files

--- commands.el
+++ commands.el
@@ -5,8 +5,27 @@
     (save-excursion
       (set-buffer buffer)
       (when (eql major-mode mode)
-	(kill-buffer buffer)))))
+        (kill-buffer buffer)))))
 
 (defun kill-image-buffers ()
   (interactive)
   (kill-mode-buffers 'image-mode))
+
+(defun current-file-extension ()
+  (file-name-extension (buffer-file-name (current-buffer))))
+
+(defun fully-indent-file (path)
+  (save-excursion
+    (with-current-buffer (find-file-noselect path)
+      (indent-region (point-min) (point-max))
+      (save-buffer)
+      (buffer-file-name))))
+
+(defun indent-directory-files ()
+  (interactive)
+  (let* ((ext (read-from-minibuffer
+               "File extension: " (current-file-extension)))
+         (ext-re (if (equal ext "") "" (format "\\.%s$" ext)))
+         (files (directory-files default-directory t ext-re)))
+    (dolist (f files nil)
+      (fully-indent-file f))))
