7b024843e0db944f0a34ba9ad8bf29c79824547c
Jeff Workman <jeff.workman@gmail.com>
2017-02-11 08:27:39 -0500

    Improvements to auto-margin.el

--- auto-margin.el
+++ auto-margin.el
@@ -5,45 +5,56 @@
   "Modified version of `split-window-sensibly' that splits horizontally
    by default when allowed."
   (interactive)
-  (let ((window (or window (selected-window))))
-    (destructuring-bind (mleft . mright) (window-margins window)
-      ;; * Remove the existing window margins first, otherwise they will
-      ;;   interfere with splitting calculations.
-      ;; * If a split is performed, it will trigger an
-      ;;   `autoset-window-margins' to set proper margin values.
-      ;; * If no split is performed, the margins are set back to
-      ;;   their existing values.
-      (set-window-margins window 0 0)
-      (or (and (window-splittable-p window t)
-               ;; Split window horizontally.
-               (with-selected-window window
-                 (split-window-right)))
-          (and (window-splittable-p window)
-               ;; Split window vertically.
-               (with-selected-window window
-                 (split-window-below)))
-          (and (eq window (frame-root-window (window-frame window)))
-               (not (window-minibuffer-p window))
-               ;; If WINDOW is the only window on its frame and is not the
-               ;; minibuffer window, try to split it vertically disregarding
-               ;; the value of `split-height-threshold'.
-               (let ((split-height-threshold 0))
-                 (when (window-splittable-p window)
-                   (with-selected-window window
-                     (split-window-below)))))
-          ;; No split, restore existing window margins
-          (and (set-window-margins window mleft mright)
-               nil)))))
+  (if (< (frame-width (window-frame window))
+         split-width-threshold)
+      ;; use the default behavior if the frame isn't wide enough to
+      ;; support two full-size horizontal windows
+      (split-window-sensibly window)
+    (let ((window (or window (selected-window))))
+      (destructuring-bind (mleft . mright) (window-margins window)
+        ;; * Remove the existing window margins first, otherwise they will
+        ;;   interfere with splitting calculations.
+        ;; * If a split is performed, it will trigger an
+        ;;   `autoset-window-margins' to set proper margin values.
+        ;; * If no split is performed, the margins are set back to
+        ;;   their existing values.
+        (set-window-margins window 0 0)
+        (or (and (window-splittable-p window t)
+                 ;; Split window horizontally.
+                 (with-selected-window window
+                   (split-window-right)))
+            (and (window-splittable-p window)
+                 ;; Split window vertically.
+                 (with-selected-window window
+                   (split-window-below)))
+            (and (eq window (frame-root-window (window-frame window)))
+                 (not (window-minibuffer-p window))
+                 ;; If WINDOW is the only window on its frame and is not the
+                 ;; minibuffer window, try to split it vertically disregarding
+                 ;; the value of `split-height-threshold'.
+                 (let ((split-height-threshold 0))
+                   (when (window-splittable-p window)
+                     (with-selected-window window
+                       (split-window-below)))))
+            ;; No split, restore existing window margins
+            (and (set-window-margins window mleft mright)
+                 nil))))))
 
 ;; Use this in place of `split-window-sensibly'
 (setq split-window-preferred-function 'split-window-prefer-horizontal)
+;;(setq split-window-preferred-function 'split-window-sensibly)
 
 ;; Set a low height threshold to generally allow vertical splits
 ;; when window is not wide enough for horizontal split.
-(setq split-height-threshold 20)
+;;(setq split-height-threshold 20)
+
+;; or set high height threshold to avoid automatic vertical splitting of
+;; both window columns?
+;; (setq split-height-threshold 80)
+
 ;; Set a high width threshold to use a horizontal split whenever
 ;; the window is wide enough.
-(setq split-width-threshold 160)
+;; (setq split-width-threshold 160)
 
 (defun autoset-window-margins (&optional window &rest args)
   (let ((w (or (and (windowp window) window)
@@ -52,7 +63,7 @@
            (mtotal (min (- ws custom-frame-width 1)
                         (- (floor (/ ws 2)) 4)))
            (ms (floor (/ mtotal 2))))
-      (if (>= ms 4)
+      (if (>= ms 7)
           (set-window-margins w ms ms)
         (set-window-margins w 0 0)))))
 (defun autoset-frame-margins (&optional frame &rest args)
