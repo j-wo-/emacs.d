91fd1bd227e5a920671c42fc2bf2c665dc34ff6d
Jeff Workman <jeff.workman@gmail.com>
2021-03-15 21:26:59 -0400

    color-theme-sanityinc-tomorrow update

--- color-theme-sanityinc-tomorrow/color-theme-sanityinc-tomorrow-pkg.el
+++ color-theme-sanityinc-tomorrow/color-theme-sanityinc-tomorrow-pkg.el
@@ -1,6 +1,10 @@
-(define-package "color-theme-sanityinc-tomorrow" "20180713.212" "A version of Chris Kempson's \"tomorrow\" themes" 'nil :keywords
+(define-package "color-theme-sanityinc-tomorrow" "20210107.2111" "A version of Chris Kempson's \"tomorrow\" themes" 'nil :commit "71b72baed2008e38ad1eb6558934e64094351cd7" :authors
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
   '("faces" "themes")
-  :url "http://github.com/purcell/color-theme-sanityinc-tomorrow")
+  :url "https://github.com/purcell/color-theme-sanityinc-tomorrow")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
